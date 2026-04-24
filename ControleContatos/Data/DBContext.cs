952dd95830c76e98c245ab41067f81fe02911700
brunof01 <bruno.fortes@unochapeco.edu.br>
2026-04-23 21:13:33 -0300

    Adicionar arquivos de projeto.

---
+++ ControleContatos/Data/DBContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class DBContext : DbContext
+{
+    public DBContext(DbContextOptions<DBContext> options)
+        : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            entity.SetTableName(entity.GetTableName().ToLower());
+            foreach (var property in entity.GetProperties())
+            {
+                property.SetColumnName(property.Name.ToLower());
+            }
+            foreach (var key in entity.GetKeys())
+            {
+                key.SetName(key.GetName().ToLower());
+            }
+            foreach (var foreignKey in entity.GetForeignKeys())
+            {
+                foreignKey.SetConstraintName(foreignKey.GetConstraintName().ToLower());
+            }
+            foreach (var index in entity.GetIndexes())
+            {
+                index.SetDatabaseName(index.GetDatabaseName().ToLower());
+            }
+        }
+    }
+
+    public DbSet<ControleContatos.Models.Contato> Contato { get; set; } = default!;
+}
---
+++ ControleContatos/Models/Contato.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleContatos.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo telefone é obrigatório")]
+        public string Telefone { get; set; }
+    }
+}
